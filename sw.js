232f81cf9529cbddd24785846ef9d5682be87440
visioapps <fanny.lecomte33@gmail.com>
2026-02-25 22:08:45 +0100

    Handle notification click and add push event listener

--- sw.js
+++ sw.js
@@ -1,3 +1,9 @@
 self.addEventListener('notificationclick', function(event) {
   event.notification.close();
+  const url = event.notification.data && event.notification.data.url ? event.notification.data.url : '/';
+  event.waitUntil(clients.openWindow(url));
+});
+
+self.addEventListener('push', function(event) {
+  // pour push future
 });
